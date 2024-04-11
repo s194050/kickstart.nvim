e2503394fccfae780dd8800ced12ff7af1b3485b
William Mathiassen <william@hybridgreentech.com>
2024-04-11 09:00:38 +0200

    Added rustaceanvim

--- init.lua
+++ init.lua
@@ -240,6 +240,11 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^4', -- Recommended
+    ft = { 'rust' },
+  },
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
